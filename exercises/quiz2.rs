20a40d48dda2356ed8b2f7017e25c9b0e51f04c5
兜兜有糖 <xidianli@qq.com>
2022-11-05 15:35:43 +0000

    commit1

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -30,11 +30,19 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Uppercase => {
+                    output.push(string.to_uppercase());
+                }
+                Trim => {
+                    output.push(string.trim().to_string());
+                }
+            }
         }
         output
     }
@@ -43,7 +51,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we have to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
